6333659f28b3805c5aa260f0b8f6c949b26353b6
Sarah Allrozamo <sarahallrozamo@gmail.com>
2020-09-13 20:57:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   argument = []
-  argument<< "array","element"
+  argument<< "array""element"
 end
 
 def add_element_to_start_of_array(array, element)
